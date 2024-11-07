6cc38cd8ebf27e7c8196c8d715c00c156e22f731
Halyson Reis <halysondasilvadosreis@gmail.com>
2024-11-07 16:30:18 -0400

    refazendo task editar perfil expositor

--- Projetos-Senac/Projeto-3-editar-perfil/js/main.js
+++
@@ -1,8 +0,0 @@
-function menuShow() {
-    let menuMobile = document.querySelector('.menu-adm .nav-bar');
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open')
-    } else {
-        menuMobile.classList.add('open')
-    }
-}
